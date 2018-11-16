0ef9478021daeefbc8d65806c89754cd02ea503b
johnyacoub <johnboshra@gmail.com>
2018-11-15 16:20:49 -0800

    v2

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var css    = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body   = document.getElementById("gradient")
-var button = document.querySelector("button");
+var button = document.querySelector(".tryme");
 
 
 function setGradient() {
